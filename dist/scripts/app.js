aea4b0467ecb83e0bd5886514d7791cdfee05b2e
smclaren727 <smclaren727@gmail.com>
2016-02-21 17:31:09 -0500

    Created templates for html pages. Addd UI-Router. Configured states and routers.

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,24 @@
-angular.module('blocjams', []);
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+  $stateProvider
+    .state('landing', {
+      url: '/',
+      templateUrl: '/templates/landing.html'
+    })
+
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/lbum.html'
+      });
+}
+
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})();
